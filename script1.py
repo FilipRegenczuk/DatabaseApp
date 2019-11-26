38eec90647de2d60a36303af99083faef9931a4e
FilipRegenczuk <f.regenczuk@outlook.com>
2019-11-26 10:51:59 +0100

    description of database and user permissions

--- script1.py
+++ script1.py
@@ -1,1 +1,12 @@
-#this is a test2
+"""
+A program that stories personal data:
+Name, Surname, Date of birth, Place of birth, Country of birth, Sex, Personal ID number
+
+User (worker) can:
+- View records
+- Search entry
+- Add entry
+- Update entry
+- Delete
+- Close
+"""
