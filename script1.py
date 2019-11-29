e10f19a8028a89cac6b6b5122ed984e569529b06
FilipRegenczuk <f.regenczuk@outlook.com>
2019-11-29 22:34:17 +0100

    frontend

--- script1.py
+++ script1.py
@@ -1,12 +1,70 @@
-"""
-A program that stories personal data:
-Name, Surname, Date of birth, Place of birth, Country of birth, Sex, Personal ID number
+import tkinter
 
-User (worker) can:
-- View records
-- Search entry
-- Add entry
-- Update entry
-- Delete
-- Close
-"""
+window = tkinter.Tk()
+
+window.title("Humans database")
+window.resizable(0, 0)
+window.geometry('700x180')
+
+# labels:
+labelName = tkinter.Label(window, text="Name:")
+labelName.grid(row=0, column=0)
+labelSurname = tkinter.Label(window, text="Surname:")
+labelSurname.grid(row=0, column=2)
+labelDate = tkinter.Label(window, text="Date of birth:")
+labelDate.grid(row=0, column=4)
+labelCountry = tkinter.Label(window, text="Country of birth:")
+labelCountry.grid(row=1, column=0)
+labelSex = tkinter.Label(window, text="Sex:")
+labelSex.grid(row=1, column=2)
+labelPesel = tkinter.Label(window, text="PESEL:")
+labelPesel.grid(row=1, column=4)
+
+# entries:
+name = tkinter.StringVar()
+entryName = tkinter.Entry(window, textvariable=name)
+entryName.grid(row=0, column=1)
+surname = tkinter.StringVar()
+entrySurname = tkinter.Entry(window, textvariable=surname)
+entrySurname.grid(row=0, column=3)
+date = tkinter.StringVar()
+entryDate = tkinter.Entry(window, textvariable=date)
+entryDate.grid(row=0, column=5)
+country = tkinter.StringVar()
+entryCountry = tkinter.Entry(window, textvariable=country)
+entryCountry.grid(row=1, column=1)
+sex = tkinter.StringVar()
+entrySex = tkinter.Entry(window, textvariable=sex)
+entrySex.grid(row=1, column=3)
+pesel = tkinter.StringVar()
+entryPesel = tkinter.Entry(window, textvariable=pesel)
+entryPesel.grid(row=1, column=5)
+
+# listbox:
+listbox = tkinter.Listbox(window, height=6, width=110)
+listbox.grid(row=2, column=0, columnspan=6)
+
+# scrollbar
+scrollbar = tkinter.Scrollbar(window)
+scrollbar.grid(row=2, column=6)
+listbox.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=listbox.yview)
+
+# buttons:
+buttonViewAll = tkinter.Button(window, text="View all", width=12)
+buttonViewAll.grid(row=3, column=0)
+buttonSearch = tkinter.Button(window, text="Search entry", width=12)
+buttonSearch.grid(row=3, column=1)
+buttonAdd = tkinter.Button(window, text="Add entry", width=12)
+buttonAdd.grid(row=3, column=2)
+buttonUpdate = tkinter.Button(window, text="Update", width=12)
+buttonUpdate.grid(row=3, column=3)
+buttonDelete = tkinter.Button(window, text="Delete", width=12)
+buttonDelete.grid(row=3, column=4)
+buttonClose = tkinter.Button(window, text="Close", width=12)
+buttonClose.grid(row=3, column=5)
+
+
+
+
+window.mainloop()
